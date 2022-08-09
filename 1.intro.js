62f8cb970b86dcb04d98ea34f637cb955a362afe
johnpathadan <johnpathadan@gmail.com>
2022-08-09 22:30:12 +0530

    global variables in node

---
+++ 1.intro.js
@@ -0,0 +1,9 @@
+const amount = 10;
+
+if(amount > 10){
+    console.log('It\'s a large number');
+}else{
+    console.log('It\'s a small number');
+}
+
+console.log(`Hey, it\'s my first node app`);
--- app.js
+++
@@ -1,9 +0,0 @@
-const amount = 10;
-
-if(amount > 10){
-    console.log('It\'s a large number');
-}else{
-    console.log('It\'s a small number');
-}
-
-console.log(`Hey, it\'s my first node app`);
