0c45b7033224e198da62d2c8a24a595d8a5c4d37
johnpathadan <johnpathadan@gmail.com>
2022-09-15 01:26:30 +0530

    event loop exit

--- app.js
+++ app.js
@@ -1,23 +1,7 @@
-const http = require('http'); //./ needed for local modules, other ones - just http
-//http is a core module, so http (http.js)
-
-const server = http.createServer(function(req, res){   //takes requestListener as an argument - 
-    //this executes for every incoming requests, 2 arguments (hover over it)
+const http = require('http');
+const server = http.createServer((req, res)=>{
     console.log(req);
+    process.exit();  //hard exited out event loop(not suitable when running websites because users cant access it now)
 }); 
 
-//function for createServer
-// function rqListener(req, res){
-
-// }
-//http.createServer(rqListener); another way of doing this
-
-//http.createServer((req, res) => {
-//    console.log(req);
-//})
-
-//this is called event driven architecture - if x happens, do y
-
-
-server.listen(3000); //listen - node will not immediately exit our script, instead keep it running to listen incoming requests
-//in chrome, if you type in localhost:3000, in VS Code terminal, it will provide a bunch of properties
+server.listen(3000); 
