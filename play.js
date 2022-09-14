5f4f578d525da306d5f8724a5adc90f7354582fa
johnpathadan <johnpathadan@gmail.com>
2022-09-14 21:39:00 +0530

    fix: end of javascript basics

--- play.js
+++ play.js
@@ -1,21 +1,42 @@
 const fetchData = () => {
-  const promise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve('Ice Cream!');
-    }, 1500);
-  });
-  return promise; //sync code, returns before setTimeOut
-};
+    const promise = new Promise((resolve, reject)=>{
+        setTimeout(()=>{
+            resolve("Ice Cream!")
+        }, 1500)
+    })
+    return promise;
+}
 
-setTimeout(() => {
-  fetchData().then(text => {
-      console.log(text);
-      return fetchData();
-  }).then(text => {
-      console.log(text);
-      return fetchData();
-  })
-}, 2000);
+setTimeout(()=>{
+    fetchData().then(text => {
+        console.log(text);
+        return fetchData();
+    }).then(text =>{
+        console.log(text);
+        return fetchData();
+    })
+}, 2000)
+console.log('coffee');
+console.log('tea');
 
-console.log("Hello");
-console.log("Sunshine");
+// const fetchData = () => {
+//   const promise = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       resolve('Ice Cream!');
+//     }, 1500);
+//   });
+//   return promise; //sync code, returns before setTimeOut
+// };
+
+// setTimeout(() => {
+//   fetchData().then(text => {
+//       console.log(text);
+//       return fetchData();
+//   }).then(text => {
+//       console.log(text);
+//       return fetchData();
+//   })
+// }, 2000);
+
+// console.log("Hello");
+// console.log("Sunshine");
